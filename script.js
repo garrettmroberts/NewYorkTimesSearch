2654776a5a05f24a5fe2dc95d080d6fb42aa6f76
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-02 13:45:41 -0600

    adjusted html id locations; constructed js query

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var urlObj = {
-  "api-key": "C7QhoZeFxrd3ER1grZm6qFmXBQ6ig2Jl",
-  "q": userSearchQuery,
-  "fl": userResultsLimit,
-};
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" + urlObj;
+var userSearchQuery;
+var userResultsLimit;
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchQuery + "&fl=" + userResultsLimit + "&api-key=C7QhoZeFxrd3ER1grZm6qFmXBQ6ig2Jl"
 
 
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-}).then(function() {
+// console.log($())
+
+$("#search").on("click", function(event) {
+  event.preventDefault();
+
+  userSearchQuery = $("#search-term").val();
+  userResultsLimit = $("#number-received").val();
+  console.log(userSearchQuery);
+  console.log(userResultsLimit)
+
+
+  event.preventDefault();
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(res) {
+    console.log(res)
+  })
+
 
 });
