eee8fcffda8d4bc953b22f8dcd1e8ca544247c0f
garrettmroberts <45220795+garrettmroberts@users.noreply.github.com>
2020-03-02 14:42:00 -0600

    Revert "populate page with basic user search"

--- script.js
+++ script.js
@@ -2,17 +2,18 @@
 var userSearchQuery;
 var userResultsLimit;
 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchQuery + "&fl=" + userResultsLimit + "&api-key=C7QhoZeFxrd3ER1grZm6qFmXBQ6ig2Jl"
 
 
 // console.log($())
 
 $("#search").on("click", function(event) {
   event.preventDefault();
-  
+
   userSearchQuery = $("#search-term").val();
   userResultsLimit = $("#number-received").val();
-  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userSearchQuery + "&limit=" + userResultsLimit + "&api-key=C7QhoZeFxrd3ER1grZm6qFmXBQ6ig2Jl"
-  console.log(queryURL)
+  console.log(userSearchQuery);
+  console.log(userResultsLimit)
 
 
   event.preventDefault();
@@ -20,16 +21,7 @@ $("#search").on("click", function(event) {
     url: queryURL,
     method: 'GET'
   }).then(function(res) {
-    var articles = res.response.docs;
-    console.log(articles)
-    articles.forEach(function(article) {
-      console.log(article)
-    var resultDiv = $("<article>");
-    var title = $("<h2>").text(article.headline.main);
-    var snippet = $("<p>").text(article.lead_paragraph);
-    resultDiv.append(title, snippet);
-    $("#top-articles").append(resultDiv);
-    })
+    console.log(res)
   })
 
 
